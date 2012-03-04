97a939403610596e6373a94ab82bb152994b68cd
trucsphamj <trucsphamj@yahoo.com>
2012-03-04 01:26:16 -0800

    done with directly play

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  @defeat = {:rock  => :scissors, :paper => :rock, :scissors => :paper}
+  @throws = @defeat.keys
+  $results = Hash.new(0)
+
+  before_filter do
+    #content_type :html
+    @defeat = {:rock  => :scissors, :paper => :rock, :scissors => :paper}
+    @throws = @defeat.keys  
+    @computer_throw = @throws.sample  
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 RockPaperScissors::Application.routes.draw do
+
+  get "throw/rock"
+
+  get "throw/paper"
+
+  get "throw/scissors"
+
+  get "throw/index"
+
+    
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +58,7 @@ RockPaperScissors::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'throw#index'
 
   # See how all your routes lay out with "rake routes"
 
